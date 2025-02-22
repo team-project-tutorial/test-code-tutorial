42ed1ccc7a97b082de6ff3a5d1a5630abb8f505f
choi-ys <project.log.062@gmail.com>
2025-02-22 11:06:32 +0900

    init: initial commit

---
+++ src/main/java/com/tutorial/testcode/TestCodeTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.testcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCodeTutorialApplication.class, args);
+	}
+
+}
