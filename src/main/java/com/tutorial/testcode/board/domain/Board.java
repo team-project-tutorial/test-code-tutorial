9ec5a74c930a29a667b5719a1cbd74f88ea15b5e
choi-ys <project.log.062@gmail.com>
2025-02-22 11:57:28 +0900

    feat: apply auditing listener to board entity

--- src/main/java/com/tutorial/testcode/board/domain/Board.java
+++ src/main/java/com/tutorial/testcode/board/domain/Board.java
@@ -3,8 +3,13 @@ package com.tutorial.testcode.board.domain;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,6 +22,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "p_board")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Board {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
@@ -28,8 +34,10 @@ public class Board {
 	@Column(nullable = false, columnDefinition = "text")
 	private String content;
 
+	@CreatedDate
 	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
+	@LastModifiedDate
 	private LocalDateTime updatedAt;
 }
